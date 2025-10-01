f59d6bee17a9fdf52dca5c8a5e0109738127b3d7
Ritik-x <133737391+Ritik-x@users.noreply.github.com>
2025-10-01 13:16:15 +0530

    userandcaptainlogin

--- client/src/pages/CaptainLogin.jsx
+++ client/src/pages/CaptainLogin.jsx
@@ -1,7 +1,85 @@
-import React from "react";
-
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 const CaptainLogin = () => {
-  return <div></div>;
+  const [email, setEmail] = useState();
+  const [password, setPassword] = useState();
+  const [data, setData] = useState();
+  const submitHandler = (e) => {
+    e.preventDefault();
+    setData({
+      email: email,
+      password: password,
+    });
+    console.log(data);
+  };
+  return (
+    <div className="flex items-center justify-center min-h-screen bg-white">
+      <form
+        onSubmit={(e) => {
+          submitHandler(e);
+        }}
+        className="bg-gray-100 rounded-xl p-10 w-96 shadow-lg space-y-6"
+      >
+        {/* Logo */}
+        <h1 className="text-3xl font-bold text-black text-center mb-6 hover:scale-105 transition-transform cursor-pointer">
+          QuickCab 🚖
+        </h1>
+
+        {/* Welcome */}
+        <h2 className="text-xl font-semibold text-center text-gray-900">
+          Sign in to your Captain account
+        </h2>
+
+        {/* Email */}
+        <div className="flex flex-col">
+          <label className="mb-1 text-gray-900 font-medium">Email</label>
+          <input
+            type="email"
+            value={email}
+            onChange={(e) => {
+              setEmail(e.target.value);
+            }}
+            required
+            placeholder="john@gmail.com"
+            className="px-4 py-3 rounded-lg bg-white border border-gray-300 focus:border-blue-500 focus:ring-1 focus:ring-blue-500 outline-none transition"
+          />
+        </div>
+
+        {/* Password */}
+        <div className="flex flex-col">
+          <label className="mb-1 text-gray-900 font-medium">Password</label>
+          <input
+            type="password"
+            value={password}
+            onChange={(e) => {
+              setPassword(e.target.value);
+            }}
+            required
+            placeholder="••••••••"
+            className="px-4 py-3 rounded-lg bg-white border border-gray-300 focus:border-blue-500 focus:ring-1 focus:ring-blue-500 outline-none transition"
+          />
+        </div>
+
+        {/* Login Button */}
+        <button
+          type="submit"
+          className="w-full hover:bg-gray-900 text-gray-100 bg-black font-semibold py-3 rounded-lg shadow-md transition transform hover:scale-[1.02] active:scale-95"
+        >
+          Login
+        </button>
+        <p className="text-center">
+          Join as Rider ? <Link className="text-blue ">Register here </Link>
+        </p>
+        {/* Captain Button */}
+        <Link
+          to="/userlogin"
+          className="w-full bg-green-300 rounded-lg text-black font-semibold px-5 py-3 shadow-md hover:bg-green-600 transition"
+        >
+          Sign in As Rider
+        </Link>
+      </form>
+    </div>
+  );
 };
 
 export default CaptainLogin;
--- client/src/pages/UserLogin.jsx
+++ client/src/pages/UserLogin.jsx
@@ -1,56 +1,83 @@
-import React from "react";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 
 const UserLogin = () => {
+  const [email, setEmail] = useState();
+  const [password, setPassword] = useState();
+  const [data, setData] = useState();
+  const submitHandler = (e) => {
+    e.preventDefault();
+    setData({
+      email: email,
+      password: password,
+    });
+    console.log(data);
+  };
   return (
-    <div className="flex items-center justify-center min-h-screen bg-black">
-      <form className="bg-gray-800 text-white rounded-xl p-10 w-96 shadow-lg space-y-6">
+    <div className="flex items-center justify-center min-h-screen bg-white">
+      <form
+        onSubmit={(e) => {
+          submitHandler(e);
+        }}
+        className="bg-gray-100 rounded-xl p-10 w-96 shadow-lg space-y-6"
+      >
         {/* Logo */}
-        <h1 className="text-3xl font-bold text-center mb-6 hover:scale-105 transition-transform cursor-pointer">
+        <h1 className="text-3xl font-bold text-black text-center mb-6 hover:scale-105 transition-transform cursor-pointer">
           QuickCab
         </h1>
 
         {/* Welcome */}
-        <h2 className="text-xl font-semibold text-center text-gray-200">
+        <h2 className="text-xl font-semibold text-center text-gray-900">
           Sign in to your account
         </h2>
 
         {/* Email */}
         <div className="flex flex-col">
-          <label className="mb-1 text-gray-300 font-medium">Email</label>
+          <label className="mb-1 text-gray-900 font-medium">Email</label>
           <input
             type="email"
+            value={email}
+            onChange={(e) => {
+              setEmail(e.target.value);
+            }}
             required
             placeholder="john@gmail.com"
-            className="px-4 py-3 rounded-lg bg-gray-400 border border-gray-600 focus:border-blue-500 focus:ring-1 focus:ring-blue-500 outline-none transition"
+            className="px-4 py-3 rounded-lg bg-white border border-gray-300 focus:border-blue-500 focus:ring-1 focus:ring-blue-500 outline-none transition"
           />
         </div>
 
         {/* Password */}
         <div className="flex flex-col">
-          <label className="mb-1 text-gray-300 font-medium">Password</label>
+          <label className="mb-1 text-gray-900 font-medium">Password</label>
           <input
             type="password"
+            value={password}
+            onChange={(e) => {
+              setPassword(e.target.value);
+            }}
             required
             placeholder="••••••••"
-            className="px-4 py-3 rounded-lg bg-gray-300 border border-gray-600 focus:border-blue-500 focus:ring-1 focus:ring-blue-500 outline-none transition"
+            className="px-4 py-3 rounded-lg bg-white border border-gray-300 focus:border-blue-500 focus:ring-1 focus:ring-blue-500 outline-none transition"
           />
         </div>
 
         {/* Login Button */}
         <button
           type="submit"
-          className="w-full bg-blue-600 hover:bg-blue-700 text-white font-semibold py-3 rounded-lg shadow-md transition transform hover:scale-[1.02] active:scale-95"
+          className="w-full hover:bg-gray-900 text-gray-100 bg-black font-semibold py-3 rounded-lg shadow-md transition transform hover:scale-[1.02] active:scale-95"
         >
           Login
         </button>
-
-        {/* Forgot / Sign up */}
-        <p className="text-center text-sm text-gray-400">
-          Don’t have an account?{" "}
-          <a href="#" className="text-blue-500 hover:underline font-medium">
-            Sign up
-          </a>
+        <p className="text-center">
+          New Here ? <Link className="text-blue "> Create New Account </Link>
         </p>
+        {/* Captain Button */}
+        <Link
+          to="/captainlogin"
+          className="w-full bg-yellow-300 rounded-lg text-black font-semibold px-5 py-3 shadow-md hover:bg-yellow-400 transition"
+        >
+          Sign in As Captain
+        </Link>
       </form>
     </div>
   );
